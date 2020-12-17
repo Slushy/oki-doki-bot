cf99829daf55fd0c8c06a9f7ec7b0451c81cfb99
Brandon Porter <brandon.porter18@gmail.com>
2020-12-16 20:01:44 -0800

    update schedule to 8am

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
     // │    │    └─────────────── hour (0 - 23)
     // │    └──────────────────── minute (0 - 59)
     // └───────────────────────── second (0 - 59, OPTIONAL)
-    schedule.scheduleJob('0 12 19 * * *', async () => {
+    schedule.scheduleJob('0 0 8 * * *', async () => {
         const channel = client.channels.get(process.env.CHANNEL);
 
         try {
